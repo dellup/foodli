d67bf100cf035434dd618f6b1d0731ae7b036722
Danil Zenin <zenindanil2000@gmail.com>
2025-08-10 23:29:42 +0300

    fix responseDTOs

--- backend/src/main/java/com/example/backend/dto/response/ApiError.java
+++ backend/src/main/java/com/example/backend/dto/response/ApiError.java
@@ -8,5 +8,4 @@ import lombok.Data;
 public class ApiError {
     private String code;
     private String message;
-
 }
--- backend/src/main/java/com/example/backend/dto/response/ApiResponse.java
+++ backend/src/main/java/com/example/backend/dto/response/ApiResponse.java
@@ -1,44 +1,18 @@
 package com.example.backend.dto.response;
 
-import com.example.backend.dto.request.ApiRequest;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.experimental.Accessors;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Data
-@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ApiResponse {
-    public static SuccessResponse success(Map<String, Object> result) {
-        var response = new SuccessResponse();
-        Optional.ofNullable(result.get("result"))
-                .map(resultValue -> (List<Optional<?>>) resultValue)
-                .ifPresent(response::setResult);
-
-        Optional.ofNullable(result.get("limit"))
-                .map(limit -> (Integer) limit)
-                .ifPresent(response::setLimit);
-
-        Optional.ofNullable(result.get("offset"))
-                .map(offset -> (Integer) offset)
-                .ifPresent(response::setOffset);
-
-        Optional.ofNullable(result.get("nextOffset"))
-                .map(nextOffset -> (Integer) nextOffset)
-                .ifPresent(response::setNextOffset);
-
-        Optional.ofNullable(result.get("total"))
-                .map(total -> (Integer) total)
-                .ifPresent(response::setTotal);
-
-        return response;
-    }
-
-    public static ErrorResponse error(List<ApiError> errors) {
-        var response = new ErrorResponse();
-        response.setErrors(errors);
-        return response;
-    }
+    private List<Optional<?>> result;
+    private Integer total;
+    private Integer nextOffset;
+    private Integer limit;
+    private Integer offset;
+    private ApiError error;
 }
